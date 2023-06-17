88674611d850963f00a85b8944d7403f5fef2790
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-17 15:16:56 -0400

    chore: lint manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,37 +7,34 @@ const manifest: Manifest.WebExtensionManifest = {
   version: pkg.version,
   description: pkg.description,
   options_ui: {
-    page: 'src/pages/options/index.html',
+    page: 'src/pages/options/index.html'
   },
   background: {
     service_worker: 'src/pages/background/index.js',
-    type: 'module',
+    type: 'module'
   },
   action: {
     default_popup: 'src/pages/popup/index.html',
-    default_icon: 'icon-34.png',
-  },
-  chrome_url_overrides: {
-    newtab: 'src/pages/newtab/index.html',
+    default_icon: 'icon-34.png'
   },
   icons: {
-    '128': 'icon-128.png',
+    '128': 'icon-128.png'
   },
-  permissions: ["activeTab"],
+  permissions: ['activeTab'],
   content_scripts: [
     {
       matches: ['http://*/*', 'https://*/*', '<all_urls>'],
       js: ['src/pages/content/index.js'],
-      css: ['contentStyle.css'],
-    },
+      css: ['contentStyle.css']
+    }
   ],
   devtools_page: 'src/pages/devtools/index.html',
   web_accessible_resources: [
     {
       resources: ['contentStyle.css', 'icon-128.png', 'icon-34.png'],
-      matches: [],
-    },
-  ],
+      matches: []
+    }
+  ]
 };
 
 export default manifest;
