d13d4aa365c4302daa0d97a1d7b0243dbad1846d
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-18 03:15:58 -0400

    chore: linting

--- src/pages/devtools/index.ts
+++ src/pages/devtools/index.ts
@@ -1,7 +1,5 @@
 import Browser from 'webextension-polyfill';
 
-Browser
-  .devtools
-  .panels
+Browser.devtools.panels
   .create('Dev Tools', 'icon-34.png', 'src/pages/panel/index.html')
   .catch(console.error);
