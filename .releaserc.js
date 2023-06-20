12481e3e74aa5418c4d088de8489d8cecf6b41a3
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-20 14:03:10 -0400

    fix: dynamically add semantic-release chrome plugin

--- .releaserc.js
+++ .releaserc.js
@@ -1,16 +1,9 @@
+// .releaserc.js
 const currentBranch =
   process.env.GITHUB_REF && process.env.GITHUB_REF.replace('refs/heads/', '');
 
-export default {
-  branches: [
-    'main',
-    {
-      name: 'develop',
-      prerelease: 'beta',
-      channel: 'beta'
-    }
-  ],
-  plugins: [
+const generatePluginsConfig = () => {
+  const basePlugins = [
     [
       '@semantic-release/commit-analyzer',
       {
@@ -21,52 +14,67 @@ export default {
           { type: 'refactor', release: 'patch' },
           { type: 'refactor', scope: 'types', release: 'minor' },
           { type: 'feat', scope: 'major', release: 'major' },
-          { scope: 'no-release', release: false }
-        ]
-      }
+          { scope: 'no-release', release: false },
+        ],
+      },
     ],
     [
       '@semantic-release/release-notes-generator',
       {
         preset: 'angular',
         parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
         },
         writerOpts: {
-          commitsSort: ['subject', 'scope']
-        }
-      }
+          commitsSort: ['subject', 'scope'],
+        },
+      },
     ],
     '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
-        npmPublish: false
-      }
-    ],
-    [
-      'semantic-release-chrome',
-      !currentBranch || currentBranch === 'main'
-        ? {
-            extensionId: 'bckfnibflpdgifdfkfoooidpblaembga',
-            target: '${{ env.CHROME_TARGET }}',
-            asset: 'masked-email-manager_v${nextRelease.version}.zip'
-          }
-        : false
+        npmPublish: false,
+      },
     ],
     [
       '@semantic-release/github',
       {
-        assets: ['masked-email-manager_v${nextRelease.version}.zip']
-      }
+        assets: ['masked-email-manager_v${nextRelease.version}.zip'],
+      },
     ],
     [
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
         message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
-  ]
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ];
+
+  if (currentBranch === 'main') {
+    // Insert before github plugin
+    basePlugins.splice(3,0,[
+      'semantic-release-chrome',
+      {
+        extensionId: 'bckfnibflpdgifdfkfoooidpblaembga',
+        asset: 'masked-email-manager_v${nextRelease.version}.zip',
+      },
+    ]);
+  }
+
+  return basePlugins;
+};
+
+export default {
+  branches: [
+    'main',
+    {
+      name: 'develop',
+      prerelease: 'beta',
+      channel: 'beta',
+    },
+  ],
+  plugins: generatePluginsConfig(),
 };
