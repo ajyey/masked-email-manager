249ac81edf1574ab4f36399473518f3348415cba
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-19 23:57:56 -0400

    feat: add new fastmail colors to tailwind.config
    This commit adds several new color options to the tailwind.config file, including 'magnesium', 'astronaut', 'charcoal', 'mikado-yellow', 'bright-sun', 'big-stone', 'french-blue' and 'malibu'.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,17 @@ module.exports = {
     extend: {
       animation: {
         'spin-slow': 'spin 20s linear infinite',
+      },
+      colors: {
+        'magnesium': '#B3B3B3',
+        'astronaut': '#3D5277',
+        'charcoal': '#404040',
+        'mikado-yellow': '#FFC107',
+        'bright-sun': '#FFD038',
+        'big-stone': '#333E48',
+        'french-blue': '#0067B9',
+        'malibu': '#7CC1F4',
+        'iron': '#D8D9D8',
       }
     },
   },
