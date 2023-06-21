bfe7d08d73bafe8f4d3702799c08df5534d938ad
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-21 01:01:05 -0400

    chore: add comments and rename variables

--- src/pages/popup/components/home/filter-pane/FilterEmailsDropdown.tsx
+++ src/pages/popup/components/home/filter-pane/FilterEmailsDropdown.tsx
@@ -88,29 +88,33 @@ function FilterEmailsDropdown({
       )
     }
   ];
+  // State for the dropdown menu open/close status
   const [isOpen, setIsOpen] = useState(false);
+  // State for the currently selected dropdown item
   const [selectedOption, setSelectedOption] = useState(dropdownItems[0]);
+  // Map the dropdown item values to their fastmail masked email state values
   const dropdownToEmailStateMap: { [key: string]: string } = {
     Enabled: 'enabled',
     Disabled: 'disabled',
     Deleted: 'deleted',
     All: 'all'
   };
+  // Toggle the dropdown menu open/close status
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
-  const selectOption = (item: any) => {
-    setSelectedOption(item);
-    setIsOpen(false);
-    onFilterChange(dropdownToEmailStateMap[item.value]);
+  // Handle the dropdown item selection
+  const selectOption = (dropdownOption: any) => {
+    setSelectedOption(dropdownOption);
+    setIsOpen(false); // Close the dropdown menu
+    onFilterChange(dropdownToEmailStateMap[dropdownOption.value]); // Update the email state filter
   };
   // Define the dropdown items with their labels, values, and SVGs
 
   return (
     <div className="relative w-[98%]">
       <button
-        id="dropdownCheckboxButton"
-        data-dropdown-toggle="dropdownDefaultCheckbox"
+        id="dropdownButton"
         className="text-white focus:outline-none font-medium rounded-lg text-sm px-4 py-1 inline-flex items-center w-full hover:bg-french-blue "
         type="button"
         onClick={toggleDropdown}
@@ -136,22 +140,21 @@ function FilterEmailsDropdown({
       {/*Render the dropdown conditionally*/}
       {isOpen && (
         <div
-          id="dropdownContainer"
+          id="dropdownOptionsContainer"
           className="absolute top-full w-full mt-1 rounded-lg text-white"
         >
-          <ul
-            className="p-1 space-y-1 text-sm bg-big-stone rounded-lg"
-            aria-labelledby="dropdownCheckboxButton"
-          >
-            {dropdownItems.map((item: any) => (
+          <ul className="p-1 space-y-1 text-sm bg-big-stone rounded-lg">
+            {dropdownItems.map((dropdownItem: any) => (
               <li
-                key={item.value}
+                key={dropdownItem.value}
                 className="flex items-center cursor-pointer hover:bg-french-blue/[0.8] rounded-lg p-2"
-                onClick={() => selectOption(item)}
+                onClick={() => selectOption(dropdownItem)}
               >
-                {item.svg}
-                <span className="text-sm font-medium">{item.label}</span>
-                {selectedOption === item.value && (
+                {dropdownItem.svg}
+                <span className="text-sm font-medium">
+                  {dropdownItem.label}
+                </span>
+                {selectedOption === dropdownItem.value && (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
