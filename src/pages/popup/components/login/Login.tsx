3d43512dab526002f11b465688619f05ab7468c7
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-21 18:06:17 -0400

    fix: css and missing onchange handler for token input

--- src/pages/popup/components/login/Login.tsx
+++ src/pages/popup/components/login/Login.tsx
@@ -68,6 +68,8 @@ export default function LoginComponent() {
                   name="api-token"
                   type="text"
                   required
+                  onChange={handleTokenChange}
+                  value={apiToken}
                   className="appearance-none relative block
                   w-full px-3 py-2 border border-gray-300
                   placeholder-gray-500 text-gray-900 rounded-md
