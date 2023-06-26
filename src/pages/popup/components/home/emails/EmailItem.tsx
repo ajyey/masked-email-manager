d467f927593237db30ee95f4adcb6be4684218cb
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-25 23:17:15 -0400

    fix: add fixed width for email items

--- src/pages/popup/components/home/emails/EmailItem.tsx
+++ src/pages/popup/components/home/emails/EmailItem.tsx
@@ -16,40 +16,23 @@ export default function EmailItem({
     onClick(maskedEmail);
   };
 
-  const truncatedEmail = useMemo(() => {
-    const emailLength = maskedEmail.email.length;
-    const maxLength = 28;
-
-    if (emailLength > maxLength) {
-      return maskedEmail.email.slice(0, maxLength - 3) + '...';
-    } else {
-      return maskedEmail.email;
-    }
-  }, [maskedEmail.email]);
-  const truncatedDescription = useMemo(() => {
-    const descriptionLength = maskedEmail.description.length;
-    const maxLength = 36;
-
-    if (descriptionLength > maxLength) {
-      return maskedEmail.description.slice(0, maxLength - 3) + '...';
-    } else {
-      return maskedEmail.description;
-    }
-  }, [maskedEmail.description]);
-
   // If the email item is currently selected, use the french-blue background color.
   const backgroundStyle = isSelected
     ? 'bg-french-blue'
     : 'hover:bg-big-stone/[0.4]';
   return (
     <div
-      className={`h-[50px] w-[95%] rounded-[5px] mx-auto m-auto ${backgroundStyle}`}
+      className={`h-[50px] w-[230px] m-1 rounded-[5px] ${backgroundStyle}`}
       onClick={handleClick}
     >
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-col align-middle ml-2 mt-1">
-          <div className="text-sm text-white font-bold">{truncatedEmail}</div>
-          <div className="text-xs text-white">{truncatedDescription}</div>
+      <div className="flex flex-row justify-between p-1.5">
+        <div className="flex flex-col align-middle truncate">
+          <div className="text-sm text-white font-bold truncate">
+            {maskedEmail.email}
+          </div>
+          <div className="text-xs text-white truncate">
+            {maskedEmail.description}
+          </div>
         </div>
       </div>
     </div>
