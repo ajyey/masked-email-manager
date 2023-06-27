0030c7a04facb08c7af939feaf43a40b42b2ce1c
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-26 21:10:00 -0400

    Revert "fix: simplify css classes for Top component using built in tailwind"
    This reverts commit 0af4aca3fe93b01c4674239a2ca81109d093513c.

--- src/pages/popup/components/home/top/Top.tsx
+++ src/pages/popup/components/home/top/Top.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import icon from '@assets/img/icon.svg';
 import SearchBar from '@pages/popup/components/home/top/SearchBar';
-import { LogoutIcon } from '@pages/popup/components/home/icons/icons';
 
 interface Props {
   onSearchChange: (searchQuery: string) => void;
@@ -20,11 +19,11 @@ export default function TopComponent({ onSearchChange, onRefresh }: Props) {
   const [isRefreshing, setIsRefreshing] = useState(false);
 
   return (
-    <section className="inline-flex h-[55px] items-center border-b border-big-stone w-full">
+    <section className="inline-flex h-[55px] items-center border-b border-b-big-stone w-full">
       <div className="w-full">
         <div className="inline-flex">
           {/*LOGO*/}
-          <div className="flex items-center">
+          <div className="flex columns-[45px] items-center">
             <a
               href="https://github.com/ajyey/masked-email-manager"
               target="_blank"
@@ -32,13 +31,13 @@ export default function TopComponent({ onSearchChange, onRefresh }: Props) {
             >
               <img
                 src={icon}
-                className="pointer-events-none h-7 w-11 ml-1.5 mr-1 mb-1"
+                className="pointer-events-none h-[30px] w-[45px] ml-1.5 mr-1 mb-1"
                 alt="icon"
               />
             </a>
           </div>
           {/*SEARCH BAR*/}
-          <div className="w-96">
+          <div className="columns-[375px]">
             <SearchBar onSearchChange={onSearchChange} />
           </div>
           {/*REFRESH BUTTON*/}
@@ -67,11 +66,11 @@ export default function TopComponent({ onSearchChange, onRefresh }: Props) {
             </button>
           </div>
           {/*CREATE EMAIL BUTTON*/}
-          <div className="flex items-center">
+          <div className="flex columns-[120px] items-center">
             <div className="relative w-full p-1 mb-1">
               <button
                 type="button"
-                className="text-white bg-french-blue font-bold rounded text-sm px-2.5 h-9 text-center inline-flex items-center"
+                className="text-white bg-french-blue font-bold rounded-[5px] text-sm p-2.5 h-[35px] text-center inline-flex items-center"
               >
                 <svg
                   width="16"
@@ -92,8 +91,22 @@ export default function TopComponent({ onSearchChange, onRefresh }: Props) {
             </div>
           </div>
           {/*HAMBURGER*/}
-          <div className="flex items-center">
-            <LogoutIcon iconClasses={'w-6 h-6 stroke-iron'} />
+          <div className="flex h-[35px] items-center ml-1.5 mt-1">
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.75 6.75H20.25M3.75 12H20.25M3.75 17.25H20.25"
+                stroke="#D8D9D8"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </div>
         </div>
       </div>
