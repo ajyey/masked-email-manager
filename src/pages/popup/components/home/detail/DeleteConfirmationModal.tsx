c99032262b09ec7b750a990ede32ba6d8fd5cfc9
Andrew Leonard <ajyey@andrewleonard.dev>
2023-06-27 23:46:25 -0400

    feat: add better looking delete confirmation modal

--- src/pages/popup/components/home/detail/DeleteConfirmationModal.tsx
+++ src/pages/popup/components/home/detail/DeleteConfirmationModal.tsx
@@ -16,35 +16,68 @@ export default function DeleteConfirmationModal({
 }: DeleteConfirmationModalProps) {
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-sm">
-          {/*content*/}
-          <div className=" rounded-lg shadow-lg relative flex flex-col w-full outline-none border-0 bg-white focus:outline-none">
-            {/*header*/}
-            <div className="flex items-center justify-center p-3 border-b border-solid border-gray-300 rounded-t">
-              <DeletedIcon iconClasses={'w-5 h-5 mr-2 stroke-red-500'} />
-              <div className="text-lg font-semibold truncate">
-                {`${selectedEmail?.email}`}
+      <div
+        id="defaultModal"
+        aria-hidden="true"
+        className="fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto md:inset-0"
+      >
+        <div className="relative w-full max-w-md">
+          {/*Modal content*/}
+          <div className="relative bg-big-stone rounded-lg shadow">
+            {/*Modal header*/}
+            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white flex items-center">
+                <DeletedIcon iconClasses={'w-5 h-5 mr-2 stroke-red-500'} />
+                <div className="text-lg font-semibold truncate">
+                  Delete Email
+                </div>
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide="defaultModal"
+                onClick={closeModal}
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+            {/*Modal body*/}
+            <div className="p-3 space-y-4">
+              <div className="text-white text-sm">
+                {`Are you sure you want to delete `}
+                <span className="font-semibold text-malibu">
+                  {selectedEmail?.email}
+                </span>
+                ?
               </div>
             </div>
-            {/*body*/}
-            <div className="flex items-center justify-center">
-              <p className="my-4 text-black text-sm">
-                Are you sure you want to delete this email?
-              </p>
-            </div>
-            {/*footer*/}
-            <div className="flex items-center justify-end p-3 border-t border-solid border-gray-300 rounded-b">
+            {/*Modal footer*/}
+            <div className="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
               <button
-                className="bg-red-500 text-white font-bold uppercase text-sm px-3 py-2 rounded-lg shadow hover:shadow-xl hover:bg-red-600 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                data-modal-hide="defaultModal"
                 type="button"
+                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 onClick={handleDelete}
               >
                 Delete
               </button>
               <button
-                className="text-gray-500 background-transparent hover:bg-gray-200 hover:shadow-xl rounded-lg font-bold uppercase px-3 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                data-modal-hide="defaultModal"
                 type="button"
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                 onClick={closeModal}
               >
                 Cancel
