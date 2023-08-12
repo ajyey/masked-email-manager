bf339af68622b1bf2158b7d2fe31b19e98e74fd8
Abhinav Gupta <mail@abhinavg.net>
2023-08-12 16:13:01 -0700

    feat(dropdown): Remember selected filter
    The filter drop down currently always uses All by default.
    This default can be annoying if you have a deleted masked emails
    that sort near the top.
    
    This changes FilterEmailsDropdown to remember the selected filter
    between invocations, so the default changes to what you selected last
    time.
    
    Note that it remembers the value only when changed with the drop down
    and not when you create a new masked email.

--- utils/constants/constants.ts
+++ utils/constants/constants.ts
@@ -1,3 +1,4 @@
 export const outputFolderName = 'dist';
 export const FASTMAIL_SESSION_KEY = 'fastmail_session';
 export const FAVORITE_EMAILS_KEY = 'favorite_emails';
+export const DEFAULT_FILTER_KEY = 'default_filter';
--- utils/storageUtil.ts
+++ utils/storageUtil.ts
@@ -1,9 +1,11 @@
 import {
   FASTMAIL_SESSION_KEY,
-  FAVORITE_EMAILS_KEY
+  FAVORITE_EMAILS_KEY,
+  DEFAULT_FILTER_KEY
 } from './constants/constants';
 import browser from 'webextension-polyfill';
 import { Session } from 'fastmail-masked-email';
+import { FILTER_OPTIONS, FilterOption } from '@src/pages/popup/components/home/filter/FilterOption';
 
 export const getFavoriteEmailIds = async (): Promise<string[]> => {
   const data = await browser.storage.sync.get(FAVORITE_EMAILS_KEY);
@@ -24,6 +26,16 @@ export const getFastmailSession = async (): Promise<Session> => {
 export const setFastmailSession = async (session: Session): Promise<void> => {
   await browser.storage.sync.set({ [FASTMAIL_SESSION_KEY]: session });
 };
+
+export const getDefaultFilter = async (): Promise<FilterOption> => {
+  const data = await browser.storage.sync.get(DEFAULT_FILTER_KEY);
+  const value = data[DEFAULT_FILTER_KEY] || 'All';
+  return FILTER_OPTIONS[value] || FILTER_OPTIONS.All;
+}
+
+export const setDefaultFilter = async (filter: FilterOption): Promise<void> => {
+  await browser.storage.sync.set({ [DEFAULT_FILTER_KEY]: filter.value });
+}
 
 export const clearStorage = async (): Promise<void> => {
   await browser.storage.sync.clear();
